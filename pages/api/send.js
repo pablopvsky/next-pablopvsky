1a4661de04bc0e7e18ba40aa8f835f7f3f599be7
Pablo Orozco <me@pablopvsky.com>
2020-09-23 22:21:19 -0500

    Update second mail text

--- pages/api/send.js
+++ pages/api/send.js
@@ -16,9 +16,13 @@ export default async function (req, res) {
   const mailConfigSecond = {
     to: email,
     from: "Pablo Orozco Montes <me@pablopvsky.com>",
-    subject: `Nuevo formulario de contacto llenado`,
-    text: message,
-    html: `<p>${message}</p>`,
+    subject: `Gracias por ponerte en contacto conmigo`,
+    text:
+      "Gracias por ponerte en contacto conmigo. Trataré de responderte lo más pronto posible.",
+    html: `<p>Gracias por ponerte en contacto conmigo. Trataré de responderte lo más pronto posible.</p>
+    <p>tu mensaje fue: ${message}</p>
+    <p>Estas recibiendo este mensaje porque me escribiste en pablopvsky.com</p>
+    `,
   };
 
   try {
