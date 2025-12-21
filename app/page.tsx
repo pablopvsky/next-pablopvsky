3fe9062c20e31afd59ae25fd7a43ef1ebf3e4683
“Pablo <me@pablopvsky.com>
2025-12-21 17:09:37 -0500

    Enhance Home component layout and styling
    - Added Image component for visual enhancement.
    - Updated Section component with new styles and background effects.
    - Included a link to "Somos Gente Digital" in the introductory paragraph for better context.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-import { Section } from "@/components/ui/Section";
 import type { Metadata } from "next";
+import Image from "next/image";
+
+import { cn } from "@/utils/class-names";
+import { Section } from "@/components/ui/Section";
 
 export const metadata: Metadata = {
   title: "Pablopvsky - Advertiser & Head of Operations",
@@ -25,14 +28,39 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="font-pablopvsky">
-      <Section container="smosh">
-        <h1>Pablopvsky</h1>
-        <p>
-          I'm a professional Advertiser and the Head of Operations at Somos
-          Gente Digital. I've been working in the industry for over 10 years. My
-          work is defined by a relentless pursuit of operational efficiency and
-          process optimization.
-        </p>
+      <Section
+        container="smosh"
+        className={cn(
+          "absolute inset-0",
+          "[background-size:20px_20px]",
+          "[background-image:radial-gradient(var(--gray-6)_1px,transparent_1px)]",
+          "dark:[background-image:radial-gradient(var(--gray-6)_1px,transparent_1px)]"
+        )}
+      >
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-[var(--gray-1)] [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)] dark:bg-[var(--gray-1)]"></div>
+        <div className="relative z-10">
+          <h1>Pablopvsky</h1>
+          <p>
+            I'm a professional Advertiser and the Head of Operations at{" "}
+            <a
+              href="https://somosgentedigital.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-accent-9 underline"
+            >
+              Somos Gente Digital
+            </a>
+            . I've been working in the industry for over 10 years. My work is
+            defined by a relentless pursuit of operational efficiency and
+            process optimization.
+          </p>
+        </div>
+        <Image
+          src="/images/keyvisual.png"
+          alt="Pablopvsky"
+          width={1000}
+          height={1000}
+        />
       </Section>
     </div>
   );
